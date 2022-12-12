c34595bfaa6d466e7e6120f6c965ec8a7315499f
mateus-lima <mtslmdcnc637@gmail.com>
2022-12-12 22:44:40 +0000

    comit 2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const fs = require("fs");
 const app = express();
 const port = 3000;
 const router = express.Router();
+const axios = require("axios");
+const nodemailer = require("nodemailer");
+
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,23 +15,87 @@ const db = () => {
         const links = fs.readFileSync('./data/links.json', 'utf-8');
         return JSON.parse(links);
     }
-router.get("/", (req, res) => {
+router.get("/home", (req, res) => {
     const links = db();
     
     res.render("index", { links });
 
 
 })
+router.get("/", (req, res) =>{
+    res.render("auth.ejs");
+})
+
+
+router.get("/sendmail", async (req, res) =>{
+
+//const { email } = req.body;
+//const number = Math.random()*10;
+
+let transporter = nodemailer.createTransport({
+    host: 'smtp.zoho.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'authentication@newtabtool.tech',
+      pass: 'uMGqxxUxRwiB',
+    },
+    tls:{
+      ciphers: "SSLv3",
+      rejectUnauthorized: false,
+    }
+  });
+  const mailOptions = {
+    from: 'Autenticação - New Tab Tool <authentication@newtabtool.tech>', // sender address
+    to: 'mtslmdcnc637@gmail.com',
+    subject: 'auth', // Subject line
+    text: 'Autenticação',
+    html:'<p>test</p>', // plain text body
+   };
+
+   await transporter.sendMail({mailOptions});
+   console.log("enviado")
+  
+
+res.send({msg: "email enviado"})
+}) // fom do req res
 
 router.post("/add", (req, res) => {
     const{link} = req.body;
     const userId = Math.random().toString(36).substr(2, 30);
     const links = db();
-    const title = link.split("/")[2];
-    links.push({title, link, userId});
+    
+
+    const options = {
+      method: 'GET',
+      url: 'https://site-scrapper.p.rapidapi.com/fetchsitetitle',
+      params: {url: link},
+      headers: {
+        'X-RapidAPI-Key': '149c43278amsh91c50075c7d36eep11f161jsn59ba85274cc7',
+        'X-RapidAPI-Host': 'site-scrapper.p.rapidapi.com'
+      }
+    
+}
+    axios.request(options).then(function (response) {
+        const title = response.data;
+        links.push({title, link, userId});
     fs.writeFileSync('./data/links.json', JSON.stringify(links), 'utf-8');
     res.redirect("/");
+    }).catch(function (error) {
+       const title = link.split("/")[2];
+       links.push({title, link, userId});
+    fs.writeFileSync('./data/links.json', JSON.stringify(links), 'utf-8');
+    res.redirect("/");
+
+    });
+
+
+
+    
 })
  
+
+
+
 app.use(router)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
