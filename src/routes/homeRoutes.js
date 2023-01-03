7cd6e87c225b3ac7d3d0e16f289132138650320f
mateus-lima <mtslmdcnc637@gmail.com>
2023-01-03 02:51:04 +0000

    bug fixed

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const bodyParser = require("body-parser");
-const userModel = require('../../models/user');
+const shortcurModel = require('../../models/shortcurModels');
 const login = require("../middleware/login");
 
 router.get("/", login, async (req, res, next) => {
 
     const id = req.user._id;
-    const links = await userModel.find({user_id: id})
+    const links = await shortcurModel.find({user_id: id})
     res.status(200).render("home.ejs", {links: links});
     next();
 
